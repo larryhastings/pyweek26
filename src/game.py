66755cc9e988192686d22641bc3aae104156eca9
Larry Hastings <larry@hastings.org>
2018-10-21 14:32:23 -0700

    Basic player movement with typematic is working.

--- src/game.py
+++ src/game.py
@@ -1,12 +1,59 @@
 #!/usr/bin/env python3
 from enum import Enum
-import pyglet.window
-from pyglet.window import key
+import pyglet.window.key as key
+import pyglet.window.key
 import sys
 import time
 
+remapped_keys = {
+    key.W: key.UP,
+    key.A: key.LEFT,
+    key.S: key.DOWN,
+    key.D: key.RIGHT,
+    key.UP: key.UP,
+    key.LEFT: key.LEFT,
+    key.DOWN: key.DOWN,
+    key.RIGHT: key.RIGHT,
+    key.ESCAPE: key.ESCAPE,
+    }
 
-map = """
+interesting_key = remapped_keys.get
+
+map_legend = {}
+
+def legend(c):
+    assert len(c) == 1
+    assert c not in map_legend
+    def decorator(klass):
+        map_legend[c] = klass
+        klass.legend = c
+        return klass
+    return decorator
+
+class MapTile:
+    pass
+
+@legend(".")
+class MapWater(MapTile):
+    pass
+
+@legend("^")
+class MapWaterCurrentUp(MapWater):
+    pass
+
+@legend("X")
+class MapBlockage(MapTile):
+    pass
+
+@legend("#")
+class MapLand(MapTile):
+    pass
+
+@legend("S")
+class MapSpawnPoint(MapTile):
+    pass
+
+map_text = """
 ......X.......................
 .##.##^.......................
 .##.##^.......................
@@ -16,8 +63,33 @@ map = """
 .S#.##^.......................
 .##.##........................
 ..............................
-""".strip()
+"""
 
+map = []
+
+for line in map_text.strip().split("\n"):
+    a = []
+    map.append(a)
+    for c in line:
+        a.append(map_legend[c]())
+
+for line in map:
+    assert len(line) == len(map[0])
+
+# the map is currently map[y][x].
+# now rotate map so x is first instead of y.
+# and index by tuple rather than nested list.
+# so that we get map[x, y]
+# (which is what tmx gives us)
+map_width = len(map[0])
+map_height = len(map)
+
+new_map = {}
+for y, line in enumerate(map):
+    for x, tile in enumerate(line):
+        new_map[x, y] = tile
+
+map = new_map
 
 class GameState(Enum):
     INVALID = 0
@@ -32,44 +104,94 @@ class GameState(Enum):
     CONFIRM_EXIT = 9
 
 
+class Ticker:
+    def __init__(self, name, hz, callback, *, delay=0):
+        self.name = name
+        self.callback = callback
+        # initial delay
+        self.delay = delay
+        self.hz = hz
+        self.reset()
+
+    # dt is fractional seconds e.g. 0.001357
+    def advance(self, dt):
+        old_elapsed = self.elapsed
+        self.elapsed += dt * self.hz
+        # print("ADVANCING", self.name, old_elapsed, "=>", self.elapsed, self.delaying, self.delay)
+        callbacks = 0
+        if self.delaying:
+            if self.elapsed < self.delay:
+                # print("TOO SOON", self.elapsed, "<", self.delay)
+                return
+            self.delaying = False
+            self.elapsed -= self.delay
+            # print("FINISHED DELAY, CALLING CALLBACK", self.name, self.callback)
+            self.callback()
+            callbacks += 1
+        floored_elapsed = int(self.elapsed)
+        while self.counter < floored_elapsed:
+            # print("CALLING CALLBACK", self.name, self.callback)
+            self.callback()
+            self.counter += 1
+            callbacks += 1
+        return callbacks
+
+    def reset(self):
+        self.counter = self.elapsed = 0
+        self.delaying = bool(self.delay)
+
+
 class Game:
     def __init__(self):
+        self.repeater = None
+
         self.start = time.time()
 
         self.old_state = self.state = GameState.INVALID
         self.transition_to(GameState.PLAYING)
 
-        self.logics = 0
-        self.logics_clock = 0
+        self.renders = Ticker("render", 4, self.render)
+        self.logics = Ticker("logic", 120, self.logic)
 
-        self.renders = 0
-        self.render_clock = 0
+        self.key_handler = self
+
+        def make_repeater(name, key):
+            def callback():
+                self.on_key(key)
+            return callback
+
+        self.repeaters = {}
+        for name, k in (
+            ("up", key.UP),
+            ("down", key.DOWN),
+            ("left", key.LEFT),
+            ("right", key.RIGHT),
+            ):
+            rk = make_repeater(name, k)
+            repeater = Ticker(name + " repeater", 4, rk, delay=1)
+            repeater.key = k
+            self.repeaters[k] = repeater
+
 
     def timer(self, dt):
-        render_delta = dt * 24
-        self.render_clock += render_delta
-        floored = int(self.render_clock)
-        while self.renders < floored:
-            self.renders += 1
-            self.render()
+        # print("TIMER", dt)
+        if self.repeater:
+            self.repeater.advance(dt)
 
         if self.state == GameState.PLAYING:
-            logics_delta = dt * 120
-            self.logics_clock += logics_delta
-            floored = int(self.logics_clock)
-            while self.logics < floored:
-                self.logics += 1
-                self.logic()
+            self.logics.advance(dt)
+
+        self.renders.advance(dt)
 
     def on_state_PLAYING(self):
-        print("playing!")
+        print("playing! ")
+        pass
 
     def transition_to(self, new_state):
         _, _, name = str(new_state).rpartition(".")
         handler = getattr(self, "on_state_" + name, None)
         if handler:
             handler()
-        sys.exit(0)
 
     def render(self):
         level.render()
@@ -77,42 +199,110 @@ class Game:
     def logic(self):
         pass
 
+    def on_key_press(self, k, modifier):
+        k = interesting_key(k)
+        if k:
+            # simulate typematic ourselves
+            # (we can't use pyglet's on_text_motion because we want this for WASD too)
+            repeater = game.repeaters.get(k)
+            if repeater:
+                repeater.reset()
+                self.repeater = repeater
+            return self.key_handler.on_key(k)
+
+    def on_key_release(self, k, modifier):
+        k = interesting_key(k)
+        if k:
+            if self.repeater and self.repeater.key == k:
+                self.repeater = None
+
+    def on_key(self, k):
+        k = interesting_key(k)
+        assert k
+        if k:
+            return self.key_handler.on_key(k)
+
 
 def clear_screen():
     print("\033[2J\033[H", end="")
+    # pass
 
 class Level:
     def __init__(self):
         self.start = time.time()
+        self.player = Player()
+        self.map = {}
+        for y in range(map_height):
+            for x in range(map_width):
+                coord = x, y
+                tile = map[coord]
+                if isinstance(tile, MapSpawnPoint):
+                    self.player.position = coord
+                    tile = MapLand()
+                self.map[coord] = tile
 
     def render(self):
         clear_screen()
         elapsed = time.time() - self.start
         print(f"{elapsed:05.1f}")
-        print(map)
+        text = []
+        for y in range(map_height):
+            for x in range(map_width):
+                coord = x, y
+                if self.player.position == coord:
+                    text.append("O")
+                    continue
+                tile = self.map[coord]
+                text.append(tile.legend)
+            text.append("\n")
+        print("".join(text))
+        print("player", self.player.position)
+        # if game.repeater:
+            # print("repeater now set for key", game.repeater.key)
 
 
 class Player:
     def __init__(self):
-        pass
+        game.key_handler = self
+
+    def on_key(self, k):
+        x, y = self.position
+        leap_x, leap_y = x, y
+        if k == key.UP:
+            y = y - 1
+            leap_y = y - 2
+        if k == key.DOWN:
+            y = y + 1
+            leap_y = y + 2
+        if k == key.LEFT:
+            x = x - 1
+            leap_x = x - 2
+        if k == key.RIGHT:
+            x = x + 1
+            leap_x = x + 2
+        new_coord = x, y
+        tile = map.get(new_coord)
+        if (not tile) or isinstance(tile, MapWater):
+            new_coord = leap_x, leap_y
+            tile = map.get(new_coord)
+            if (not tile) or isinstance(tile, MapWater):
+                return
+        self.position = new_coord
 
 
 
 window = pyglet.window.Window()
 game = Game()
 level = Level()
-player = Player()
 
-up_keys =     frozenset((key.UP,    key.W,))
-left_keys =   frozenset((key.LEFT,  key.A,))
-down_keys =   frozenset((key.DOWN,  key.S,))
-right_keys =  frozenset((key.RIGHT, key.D,))
-escape_keys = frozenset((key.ESCAPE,))
 
 @window.event
-def on_key_press(symbol, modifiers):
-    if symbol in up_keys:
-        print("UP")
+def on_key_press(key, modifiers):
+    return game.on_key_press(key, modifiers)
+
+@window.event
+def on_key_release(key, modifiers):
+    return game.on_key_release(key, modifiers)
 
 
 def timer_callback(dt):
