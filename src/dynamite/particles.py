8f8398b2eeb55027791e42f9e56465c48245d3b0
Daniel Pope <mauve@mauveweb.co.uk>
2018-10-26 21:14:09 +0100

    Improve ripples

--- src/dynamite/particles.py
+++ src/dynamite/particles.py
@@ -8,13 +8,14 @@ class FlowParticles:
     """Particles that indicate the flow of water."""
 
     SPEED = 0.6
-    DRAG = 0.3
+    DRAGY = 0.3
+    DRAGX = 0.5
 
     @classmethod
     def load(cls):
         cls.ripple = pyglet.resource.image('ripple.png')
         cls.ripple.anchor_x = cls.ripple.width * 0.5
-        cls.ripple.anchor_y = cls.ripple.height * 0.5
+        cls.ripple.anchor_y = 0
 
     def __init__(self, level):
         self.level = level
@@ -32,14 +33,16 @@ class FlowParticles:
 
         new_particles = []
         for p in self.particles:
-            p.age += dt * 2
-            if p.age > 4:
+            p.age += dt
+            if p.age > 2:
                 continue
 
             if p.age < 1:
                 p.opacity = p.age * p.bright
-            elif p.age > 3:
-                p.opacity = (4 - p.age) * p.bright
+                p.scale_y = p.age * p.max_scale * 0.8 + 0.2
+            elif p.age > 1:
+                p.opacity = (2 - p.age) * p.bright
+                p.scale_y = (2 - p.age) * 0.8 * p.max_scale + 0.2
 
             x, y = p.map_pos
             current = water_tiles.get((round(x), round(y)))
@@ -49,9 +52,11 @@ class FlowParticles:
 
             vx, vy = p.v
 
-            frac = self.DRAG ** dt
+            frac = self.DRAGX ** dt
             invfrac = 1.0 - frac
             vx = frac * vx + invfrac * curx
+            frac = self.DRAGY ** dt
+            invfrac = 1.0 - frac
             vy = frac * vy + invfrac * cury
 
             p.v = vx, vy
@@ -63,7 +68,10 @@ class FlowParticles:
             new_particles.append(p)
 
         for (tx, ty), current in water_tiles.items():
-            if random.uniform(0, 3) > dt:
+            cx, cy = current
+            f = cx + cy
+
+            if random.uniform(0, 1 if f else 2) > dt:
                 continue
             x = random.uniform(tx - 0.5, tx + 0.5)
             y = random.uniform(ty - 0.5, ty + 0.5)
@@ -81,10 +89,12 @@ class FlowParticles:
             cx, cy = current
             p.v = (
                 cx + random.uniform(-0.5, 0.5),
-                cy + random.uniform(-0.5, 0.5)
+                cy + random.uniform(-0.2, 0.2)
             )
             p.opacity = 0
-            p.scale = random.uniform(0.2, 0.3)
+            p.max_scale = random.uniform(0.3, 0.6)
+            p.scale_x = p.max_scale
+            p.scale_y = 0.2
             p.map_pos = map_pos
             new_particles.append(p)
         self.particles = new_particles
